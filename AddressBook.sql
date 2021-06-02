b5ac416dde6db936956f6697a0c014b5a8282e30
joo7998 <joo7998@gmail.com>
2021-06-02 14:46:35 +0900

    insert

--- AddressBook.sql
+++ AddressBook.sql
@@ -14,7 +14,7 @@ CREATE TABLE phone_book (
     tel VARCHAR2(30) NOT NULL     
 );
 
-DESC phone_book;
+SELECT * FROM phone_book;
 
 CREATE SEQUENCE seq_phone_book_pk
     START WITH 1
@@ -22,5 +22,22 @@ CREATE SEQUENCE seq_phone_book_pk
     MINVALUE 1
     MAXVALUE 100000000
     NOCACHE;
+    
+SELECT * FROM USER_SEQUENCES;
 
---commit push test
+INSERT INTO phone_book VALUES (1, 'Coco', '010-10**-23**','02-43**-90**');
+INSERT INTO phone_book VALUES (2, 'Nana', '010-55**-55**','02-43**-90**');
+INSERT INTO phone_book VALUES (3, 'Micheal', '010-99**-77**','02-43**-90**');
+INSERT INTO phone_book VALUES (4, 'Sam', '010-88**-90**','02-43**-90**');
+INSERT INTO phone_book VALUES (5, 'Park', '010-98**-14**','02-9**-98**');
+
+COMMIT;
+
+SELECT * FROM phone_book;
+
+DESC phone_book
+
+
+
+
+
